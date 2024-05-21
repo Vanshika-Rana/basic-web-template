8d9e6d16558382735bae3576bcd2e90fec93d471
Vanshika Rana <ranavanshika172000@gmail.com>
2024-05-21 17:10:54 +0530

    added: gradient

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className='bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-indigo-900 to-gray-950 flex flex-col items-center justify-center min-h-screen p-6 sm:p-12 md:p-24'>
+		<main className='animated-background bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-indigo-950 to-gray-950 flex flex-col items-center justify-center min-h-screen p-6 sm:p-12 md:p-24'>
 			<h1 className='text-4xl sm:text-5xl md:text-7xl font-black text-indigo-300 text-center'>
 				UPSURGE LABS
 			</h1>
@@ -12,10 +12,10 @@ export default function Home() {
 				purus nec nibh auctor at massa.
 			</p>
 			<div className='flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-6 pt-6'>
-				<button className='bg-indigo-600 px-6 py-2 sm:px-8 sm:py-2 rounded font-bold text-base sm:text-lg text-white transition transform hover:scale-95'>
+				<button className='bg-indigo-600 px-6 py-2 sm:px-8 sm:py-2 rounded font-bold text-base sm:text-lg text-white transition ease-in-out duration-500 hover:scale-95'>
 					About Us
 				</button>
-				<button className='bg-white px-6 py-2 sm:px-8 sm:py-2 rounded font-bold text-base sm:text-lg text-indigo-600 transition transform hover:scale-95'>
+				<button className='bg-white px-6 py-2 sm:px-8 sm:py-2 rounded font-bold text-base sm:text-lg text-indigo-600 transition ease-in-out duration-500 hover:scale-95'>
 					Get In Touch
 				</button>
 			</div>
